da194c3372d5bc1afe39261a2e2d89dfa317d7ea
Luchin111 <luisinmedina@gmail.com>
2020-10-15 14:43:21 -0400

    Changing Bl Reset password

--- piratebay-backend-1/src/main/java/bo/edu/ucb/sis/piratebay/controller/ResetPasswordRequestController.java
+++ piratebay-backend-1/src/main/java/bo/edu/ucb/sis/piratebay/controller/ResetPasswordRequestController.java
@@ -1,5 +1,7 @@
 package bo.edu.ucb.sis.piratebay.controller;
 
+import bo.edu.ucb.sis.piratebay.bl.RegisterProviderBl;
+import bo.edu.ucb.sis.piratebay.bl.ResetPasswordRequestBl;
 import bo.edu.ucb.sis.piratebay.model.ProductsModel;
 import bo.edu.ucb.sis.piratebay.model.ResetPasswordRequestModel;
 import com.auth0.jwt.JWT;
@@ -8,6 +10,7 @@ import com.auth0.jwt.interfaces.DecodedJWT;
 import com.auth0.jwt.interfaces.JWTVerifier;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -28,54 +31,22 @@ import java.util.Properties;
 @RequestMapping("/api/v1/resetpassword")
 @CrossOrigin(origins = "*")
 public class ResetPasswordRequestController {
-    @Value("${spring.mail.username}")
-    private String username;
-    @Value("${spring.mail.password}")
-    private String password;
+
+    private ResetPasswordRequestBl resetPasswordRequestBl;
+    @Autowired
+    public ResetPasswordRequestController(ResetPasswordRequestBl resetPasswordRequestBl) {
+        this.resetPasswordRequestBl = resetPasswordRequestBl;
+
+
+    }
 
     @RequestMapping(method = RequestMethod.POST,
             produces = MediaType.APPLICATION_JSON_VALUE)
     public String sendEmail(@RequestBody ResetPasswordRequestModel resetPasswordRequestModel) throws IOException, MessagingException {
 
-        sendmail(resetPasswordRequestModel);
+        resetPasswordRequestBl.sendmail(resetPasswordRequestModel);
         return "Email sent successfully";
     }
 
-    private void sendmail(ResetPasswordRequestModel resetPasswordRequestModel) throws MessagingException, IOException {
 
-        Properties props= new Properties();
-        props.put("mail.smtp.auth", "true");
-        props.put("mail.smtp.starttls.enable", "true");
-        props.put("mail.smtp.host", "smtp.gmail.com");
-        props.put("mail.smtp.port", "587");
-
-        Session session= Session.getInstance(props, new javax.mail.Authenticator(){
-           protected PasswordAuthentication getPasswordAuthentication(){
-               return new PasswordAuthentication(username,password);
-           }
-        });
-
-        Message msg = new MimeMessage(session);
-        msg.setFrom(new InternetAddress(username, false));
-
-        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(resetPasswordRequestModel.getEmail()));
-        msg.setSubject(resetPasswordRequestModel.getSubject());
-        msg.setContent(resetPasswordRequestModel.getBody(), "text/html");
-        msg.setSentDate(new Date());
-
-        MimeBodyPart messageBodyPart = new MimeBodyPart();
-        messageBodyPart.setContent(resetPasswordRequestModel.getBody(), "text/html");
-
-        Multipart multipart = new MimeMultipart();
-        multipart.addBodyPart(messageBodyPart);
-        MimeBodyPart attachPart = new MimeBodyPart();
-
-        attachPart.attachFile("C:\\Users\\Luchin111\\Desktop\\perfil.jpeg");
-
-        multipart.addBodyPart(attachPart);
-        msg.setContent(multipart);
-        // sends the e-mail
-        Transport.send(msg);
-
-    }
 }
