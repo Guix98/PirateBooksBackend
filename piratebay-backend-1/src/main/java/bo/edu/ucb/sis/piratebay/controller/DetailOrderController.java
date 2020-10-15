2d9c8198e7d846d6705fd7519486bf77fb2d142b
camilemuz <iandope20@gmail.com>
2020-10-15 15:00:54 -0400

    detalle de pedido

---
+++ piratebay-backend-1/src/main/java/bo/edu/ucb/sis/piratebay/controller/DetailOrderController.java
@@ -0,0 +1,63 @@
+package bo.edu.ucb.sis.piratebay.controller;
+
+import bo.edu.ucb.sis.piratebay.bl.DetailOrderBl;
+import bo.edu.ucb.sis.piratebay.bl.OrderBl;
+import bo.edu.ucb.sis.piratebay.model.DetailOrderModel;
+import bo.edu.ucb.sis.piratebay.model.OrderModel;
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.auth0.jwt.interfaces.JWTVerifier;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/order/id")
+@CrossOrigin(origins = "*")
+public class DetailOrderController {
+
+    private DetailOrderBl detailOrderBl;
+
+    @Value("${piratebay.security.secretJwt}")
+    private String secretJwt;
+
+    @Autowired
+    public DetailOrderController(DetailOrderBl detailOrderBl) {
+        this.detailOrderBl= detailOrderBl;
+    }
+
+    @RequestMapping(method = RequestMethod.POST,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<DetailOrderModel>> findOrder(@RequestHeader("Authorization") String authorization, @RequestBody String json) throws JSONException { // bearer asdasdasdasd
+
+        // Lo unico que estamos haciendo es decodificar el token.
+        String tokenJwT = authorization.substring(7);
+        System.out.println("TOKEN JWT: " + tokenJwT);
+        DecodedJWT decodedJWT = JWT.decode(tokenJwT);
+        String idUsuario = decodedJWT.getSubject();
+        System.out.println("USUARIO: " + idUsuario);
+        JSONObject jsonObject = new JSONObject(json);
+        int order = Integer.parseInt(jsonObject.getString("orderName"));
+        System.out.println("ORDER_NAME: " + order);
+        if(!"AUTHN".equals(decodedJWT.getClaim("type").asString()) ) {
+            throw new RuntimeException("El token proporcionado no es un token de Autenthication");
+        }
+        // El siguiente código valida si el token es bueno y ademas es un token de authentication
+
+        Algorithm algorithm = Algorithm.HMAC256(secretJwt);
+        JWTVerifier verifier = JWT.require(algorithm)
+                .withIssuer("PirateBay")
+                .build();
+        verifier.verify(tokenJwT);
+
+        return new ResponseEntity<>( this.detailOrderBl.findOrder(order), HttpStatus.OK);
+    }
+}
