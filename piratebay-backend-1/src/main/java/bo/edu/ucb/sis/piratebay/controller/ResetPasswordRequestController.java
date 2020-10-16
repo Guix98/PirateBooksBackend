f2382cd952e5b675faff132f333faea00c4d2295
Luchin111 <luisinmedina@gmail.com>
2020-10-16 00:07:47 -0400

    Reset password Controller Complete

--- piratebay-backend-1/src/main/java/bo/edu/ucb/sis/piratebay/controller/ResetPasswordRequestController.java
+++ piratebay-backend-1/src/main/java/bo/edu/ucb/sis/piratebay/controller/ResetPasswordRequestController.java
@@ -1,31 +1,16 @@
 package bo.edu.ucb.sis.piratebay.controller;
 
-import bo.edu.ucb.sis.piratebay.bl.RegisterProviderBl;
 import bo.edu.ucb.sis.piratebay.bl.ResetPasswordRequestBl;
-import bo.edu.ucb.sis.piratebay.model.ProductsModel;
 import bo.edu.ucb.sis.piratebay.model.ResetPasswordRequestModel;
-import com.auth0.jwt.JWT;
-import com.auth0.jwt.algorithms.Algorithm;
-import com.auth0.jwt.interfaces.DecodedJWT;
-import com.auth0.jwt.interfaces.JWTVerifier;
 import org.json.JSONException;
-import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeBodyPart;
-import javax.mail.internet.MimeMessage;
-import javax.mail.internet.MimeMultipart;
 import java.io.IOException;
-import java.util.Date;
-import java.util.List;
-import java.util.Properties;
 
 @RestController
 @RequestMapping("/api/v1/resetpassword")
@@ -40,12 +25,13 @@ public class ResetPasswordRequestController {
 
     }
 
+
     @RequestMapping(method = RequestMethod.POST,
             produces = MediaType.APPLICATION_JSON_VALUE)
-    public String sendEmail(@RequestBody ResetPasswordRequestModel resetPasswordRequestModel) throws IOException, MessagingException {
+    public ResponseEntity findUserByEmail(@RequestBody ResetPasswordRequestModel resetPasswordRequestModel) throws JSONException, IOException, MessagingException {
 
         resetPasswordRequestBl.sendmail(resetPasswordRequestModel);
-        return "Email sent successfully";
+        return new ResponseEntity<>( this.resetPasswordRequestBl.finUserByEmail(resetPasswordRequestModel.getEmail()), HttpStatus.OK);
     }
 
 
