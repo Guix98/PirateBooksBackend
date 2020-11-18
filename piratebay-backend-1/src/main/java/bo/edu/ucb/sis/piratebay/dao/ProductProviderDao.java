60e173364e9e6bb2d4cec11817c14466d5d14fa2
camilemuz <iandope20@gmail.com>
2020-11-18 17:51:31 -0400

    arreglo de productos por proveedor

--- piratebay-backend-1/src/main/java/bo/edu/ucb/sis/piratebay/dao/ProductProviderDao.java
+++ piratebay-backend-1/src/main/java/bo/edu/ucb/sis/piratebay/dao/ProductProviderDao.java
@@ -21,12 +21,12 @@ public class ProductProviderDao {
 
     public List<ProductProviderModel> findAllProduct(int id_provider) {
 
-       String query =  "SELECT x.product_code , x.product_name,  x.unit_price, y.category_name FROM product x, category y, provider z WHERE x.category_id = y.category_id AND x.provider_id = z.provider_id AND z.provider_id ="+id_provider+";";
-       /* String query = "SELECT x.product_code , x.product_name,  x.author, x.unit_price, y.category_name\n " +
+       //String query =  "SELECT x.product_code , x.product_name,  x.author, x.unit_price, y.category_name FROM product x, category y, provider z WHERE x.category_id = y.category_id AND x.provider_id = z.provider_id AND z.provider_id = 1;";
+       String query = "SELECT x.product_code , x.product_name,  x.author, x.unit_price, y.category_name\n " +
                 "                FROM product x, category y, provider z\n " +
                 "                WHERE x.category_id = y.category_id\n " +
                 "                AND x.provider_id = z.provider_id\n " +
-                "                AND z.provider_id = "+id_provider+";";*/
+                "                AND z.provider_id = "+id_provider+";";
         List<ProductProviderModel> result = null;
         try {
             result = jdbcTemplate.query(query, new RowMapper<ProductProviderModel>() {
@@ -36,7 +36,7 @@ public class ProductProviderDao {
                             resultSet.getString(1),
                             resultSet.getString(2),
                             resultSet.getString(3),
-                            resultSet.getDouble(3),
+                            resultSet.getDouble(4),
                             resultSet.getString(5));
 
                 }
